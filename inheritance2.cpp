ab604e2746e09c10bfca2de3e6469e13ec3850c8
Manpreet Mann <manpreetmann4788@gmail.com>
2024-06-15 22:44:58 +0530

    learning

--- inheritance2.cpp
+++ inheritance2.cpp
@@ -1,6 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    
+class Student
+{
+public:
+    string name;
+    int id;
+};
+class Btech : public Student
+{
+public:
+    string subject;
+};
+
+int main()
+{
+    Btech obj;
+    obj.name = "Manpreet";
+    obj.id = 2;
+    obj.subject = "english";
+    cout << obj.name << endl;
+    cout << obj.id << endl;
+    cout << obj.subject << endl;
+    // manpreet and 2 is stored in the copy of name and id in the derived class not the original class
     return 0;
 }
